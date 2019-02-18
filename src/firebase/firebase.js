d9b101463ff6df392cf8484e53caf65d72359c58
xuyangzo <xuyangzo@usc.edu>
2019-02-17 17:05:46 -0800

    Add Environment Variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,14 @@
-import * as firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+require("dotenv").config();
 
 const config = {
-  apiKey: "AIzaSyA3Qmzg73Y43rEHcqa_x1dFuUlo4GG-KGY",
-  authDomain: "website-react-69b18.firebaseapp.com",
-  databaseURL: "https://website-react-69b18.firebaseio.com",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
   projectId: "website-react-69b18",
-  storageBucket: "website-react-69b18.appspot.com",
-  messagingSenderId: "819408276949"
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDERID
 };
 firebase.initializeApp(config);
 
