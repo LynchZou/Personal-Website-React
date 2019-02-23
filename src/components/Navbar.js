9998389ec5075aebedbba72481b25fd5b481b3c9
xuyangzo <xuyangzo@usc.edu>
2019-02-22 16:43:48 -0800

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,8 @@ class Navbar extends React.Component {
     super(props);
     this.state = {
       showSidebar: false,
-      display: "none"
+      display: "none",
+      disableHeadroom: window.screen.width <= 420
     };
   }
 
@@ -40,15 +41,11 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar-container top">
-        <Headroom
-          style={{
-            maxHeight: "75px",
-            display: this.state.display,
-            background: "transparent"
-          }}
-          onPin={() => this.setState({ display: "block" })}
-        >
-          <nav className="navbar navbar-expand-lg transparent navbar-inverse justify-content-between ">
+        {this.state.disableHeadroom ? (
+          <nav
+            className="navbar navbar-expand-lg transparent navbar-inverse justify-content-between "
+            style={{ display: "block" }}
+          >
             <div className="logo" onClick={this.onClickLogo}>
               <img
                 src={logo}
@@ -79,7 +76,48 @@ class Navbar extends React.Component {
               />
             </div>
           </nav>
-        </Headroom>
+        ) : (
+          <Headroom
+            style={{
+              maxHeight: "75px",
+              display: this.state.display,
+              background: "transparent"
+            }}
+            onPin={() => this.setState({ display: "block" })}
+          >
+            <nav className="navbar navbar-expand-lg transparent navbar-inverse justify-content-between ">
+              <div className="logo" onClick={this.onClickLogo}>
+                <img
+                  src={logo}
+                  className="ml-5 nav-logo"
+                  style={{ width: "50px" }}
+                />
+                Lynch
+              </div>
+              <div className="nav-right">
+                <div class="dropdown mr-3">
+                  <button class="dropbtn">
+                    {this.props.language.language == "en"
+                      ? "English"
+                      : "简体中文"}
+                  </button>
+                  <div class="dropdown-content">
+                    <p href="#" onClick={this.onChangeLanguage}>
+                      {this.props.language.language == "ch"
+                        ? "English"
+                        : "简体中文"}
+                    </p>
+                  </div>
+                </div>
+                <i
+                  className="fas fa-bars mr-5 sidebar-icon"
+                  onClick={this.onClickBar}
+                  ref={node => (this.node = node)}
+                />
+              </div>
+            </nav>
+          </Headroom>
+        )}
       </div>
     );
   }
